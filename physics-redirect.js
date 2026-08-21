bdfb168b2741fdef66cb3769ec96d2ed76f4fa8c
zycaonirl <ac.tapayanemmanuellexis@gmail.com>
2026-08-21 11:58:20 +0800

    random bullstuff go

--- physics-redirect.js
+++ physics-redirect.js
@@ -1,1 +1,4 @@
-window.location.replace("https://lexis-physics2026.blogspot.com/");
+window.location.replace("https://lexis-physics2026.blogspot.com/")
+document.addEventListener("DOMContentLoaded", () =>
+  window.location.replace("https://lexis-physics2026.blogspot.com/")
+);
